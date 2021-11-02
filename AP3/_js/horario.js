3d395a4c66c25122fd1be2cf507c43eb4dd46bcf
Raul Mendes <90268686+raulmendes93@users.noreply.github.com>
2021-11-02 13:01:24 -0300

    Delete AP3 directory

--- AP3/_js/horario.js
+++
@@ -1,7 +0,0 @@
-function carregar(){
-    var msg = window.document.getElementById("msg");
-    var data = new Date();
-    var hora = data.getHours();
-    var min = data.getMinutes();
-    msg.innerHTML = `Informação atualizada às ${hora}:${min}.`
-}
